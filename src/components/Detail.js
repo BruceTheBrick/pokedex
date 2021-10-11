064d32a0d3d5f5a75288e3081a91cd027ba7f3fd
DrTardis1 <techbryce@hotmail.com>
2021-10-11 20:15:38 +1100

    Added togglable 3d spin effect to pokemon sprite

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,17 +1,35 @@
+import React from "react";
 import "./Detail.css";
-const Detail = ({ route, navigation }) => {
-  const { pokemon } = route.params;
-  return (
-    <div className="content">
-      <button onClick={() => navigation.goBack()}>Return</button>
-      <div className="pokemon">
-        <div className="pokemon-img">
-          <img src={pokemon.imgURL} alt="" />
-          <div className="sprite-shadow"></div>
+class Detail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isSpinning: true };
+  }
+
+  updateSpin = () => {
+    this.setState({
+      isSpinning: !this.state.isSpinning,
+    });
+    console.log(this.state.isSpinning);
+  };
+
+  render() {
+    const { pokemon } = this.props.route.params;
+    return (
+      <div className="content">
+        <button onClick={() => this.props.navigation.goBack()}>Return</button>
+        <div className="pokemon">
+          <div className={"pokemon-img " + (this.state.isSpinning ? "spin-3d" : "")}>
+            <img src={pokemon.imgURL} alt={"Sprite of " + pokemon} />
+            <div className="sprite-shadow"></div>
+          </div>
+        </div>
+        <div>
+          <input type="checkbox" name="isSpinning" id="isSpinning" onClick={this.updateSpin} />
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Detail;
