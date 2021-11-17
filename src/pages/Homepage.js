dfbc74e371c99039bd61a34520f4d2c36f3cdf5c
DrTardis1 <35167644+DrTardis1@users.noreply.github.com>
2021-11-17 21:02:24 +1100

    Still struggling to understand how to update a list when state updates

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -5,6 +5,9 @@ import List from "../components/List";
 import PokeDexLogo from "../assets/PokeDex.png";
 import Pagination from "../components/Pagination";
 
+import PokemonItem from "../components/PokemonItem";
+import { NavLink } from "react-router-dom";
+
 const PokeDexAPI = require("pokeapi-js-wrapper");
 
 export default class Homepage extends Component {
@@ -21,9 +24,22 @@ export default class Homepage extends Component {
     this.pokedex = new PokeDexAPI.Pokedex(options);
     this.pokemonUpdated = this.pokemonUpdated.bind(this);
 
-    this.state = {
-      list: "",
-    };
+    this.startIndex = 0;
+    this.limit = 10;
+    this.totalCount = 0;
+
+    this.getNext = this.getNext.bind(this);
+  }
+
+  componentDidUpdate() {
+    console.log("Homepage updating");
+    console.log(this.state);
+  }
+
+  async getNext() {
+    let newList = await this.pokedex.getPokemonsList({ offset: this.startIndex, limit: this.limit });
+    this.startIndex += this.limit;
+    this.setState({ list: newList.results });
   }
 
   pokemonUpdated(newList) {
@@ -31,10 +47,24 @@ export default class Homepage extends Component {
   }
 
   render() {
+    console.log(this?.state?.list);
     return (
       <Page title="Pokedex" image={PokeDexLogo} image_alt={"PokeDex"}>
-        <List list={this.state.list} pokedex={this.pokedex} />
-        <Pagination limit={10} startIndex={0} listUpdated={this.pokemonUpdated} pokedex={this.pokedex}></Pagination>
+        <button onClick={this.getNext}>Get Next</button>
+        {this?.state?.list.map((pokemon) => {
+          console.log(pokemon);
+          <NavLink
+            to={{
+              pathname: "/PokemonDetails",
+              state: pokemon,
+            }}
+            key={pokemon.id}
+          >
+            <PokemonItem pokemon={pokemon} pokedex={this.pokedex} />
+          </NavLink>;
+        })}
+        {/* <List list={this?.state?.list} pokedex={this.pokedex} /> */}
+        {/* <Pagination limit={10} startIndex={0} listUpdated={this.pokemonUpdated} pokedex={this.pokedex}></Pagination> */}
       </Page>
     );
   }
