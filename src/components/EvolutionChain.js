d2f5628bf9407c79b36ceab19a799ddfccc16d57
Bryce <techbryce@hotmail.com>
2022-02-03 20:32:39 +1100

    Added backend functionality to get a list of pokemon evolution chain

--- src/components/EvolutionChain.js
+++ src/components/EvolutionChain.js
@@ -7,17 +7,38 @@ export default class EvolutionChain extends Component {
 
     this.pokemon = props.pokemon;
     this.pokedex = props.pokedex;
-    this.parseEvoChain();
+    this.getEvoChain();
   }
 
-  async parseEvoChain() {
-    this.evoChain = await this.pokedex.getEvolutionChainById(this.pokemon.id);
-    console.log(this.evoChain);
+  async getEvoChain() {
+    this.species = await this.pokedex.getPokemonSpeciesByName(this.pokemon.species.name);
+    this.evoChain = await this.pokedex.getEvolutionChainById(this.getEvolutionChainId(this.species.evolution_chain.url));
+    this.initEvolutions();
   }
+
+  initEvolutions() {
+    let evo = this.evoChain.chain;
+    let pokemon = [];
+    pokemon.push(evo.species);
+    do {
+      let numEvos = evo.evolves_to.length;
+      pokemon.push(evo.evolves_to[0].species);
+      if (numEvos > 1) {
+        for (let i = 1; i < numEvos; i++) {
+          pokemon.push(evo.evolves_to[i].speces);
+        }
+      }
+      evo = evo.evolves_to[0];
+    } while (evo.evolves_to.length > 0);
+
+    console.log(pokemon);
+  }
+
+  getEvolutionChainId(chainURL) {
+    return chainURL.substring(chainURL.indexOf("/", chainURL.length - 5) + 1, chainURL.length - 1);
+  }
+
   render() {
     return <div>Testing!</div>;
-    // return this.parseEvoChain().map((pokemon) => {
-    //   return <div>{pokemon.id}</div>;
-    // });
   }
 }
